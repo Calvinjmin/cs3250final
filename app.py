95afebd97fc7aab57398d28e9fe6413af7adab7f
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2022-04-19 17:06:12 -0400

    Database:

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 from index import idx
 from auth import bp
+import db
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 app.register_blueprint(idx)
 app.register_blueprint(bp)
+db.init_app(app)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
