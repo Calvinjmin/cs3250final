4035bae1f1f2b85f75aa5b60d38b07d56f6cbf93
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2022-04-19 17:16:56 -0400

    Database + Init App changes

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 import os
 from flask import Flask
-from index import idx
-from auth import bp
-import db
 
-app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-)
-app.register_blueprint(idx)
-app.register_blueprint(bp)
-db.init_app(app)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    import db
+    db.init_app(app)
+
+    import auth
+    app.register_blueprint(auth.bp)
+
+    import index
+    app.register_blueprint(index.idx)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
